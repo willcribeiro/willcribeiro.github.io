75a11ee97fd2593429270f20132fa0f522bbb59f
willcribeiro <william-cribeiro@bct.ect.ufrn.br>
2019-01-16 19:36:52 -0300

    Terminal implement

--- js/script.js
+++ js/script.js
@@ -23,13 +23,23 @@
 			if (text.indexOf("help")<2 && text.indexOf("help")>-1) { 				//checar comando "help"
 				$("#terminal").append("<br><span class='terminal-blue'>cd about - </span><span class='terminal-green'>for info about me</span><br>");
 				$("#terminal").append("<span class='terminal-blue'>cd projects - </span><span class='terminal-green'>for info about my projects</span><br>");
-				$("#terminal").append("<span class='terminal-blue'><a href='#'' class='terminal-blue'>cd homepage</a> - </span><span class='terminal-green'>exit this terminal and go to my homepage</span><br>");
+				$("#terminal").append("<span class='terminal-blue'><a ' class='terminal-blue'>cd homepage</a> - </span><span class='terminal-green'>exit this terminal and go to my homepage</span><br>");
 			}else if (text.indexOf("cd homepage")<2 && text.indexOf("cd homepage")>-1){   //checa a entrada do usuario
-				$("#terminal").append("<br><span class='terminal-red'>homepage not inplemented yet</span>");
+				$("#terminal").append("<br><span class='terminal-red'>To go to my homepage <a href='index.html'' class='terminal-red'>Click here</a> and i hope to see you later :)</span>");
+			
+			}else if (text.indexOf("cd projects")<2 && text.indexOf("cd projects")>-1){   //checa a entrada do usuario
+				$("#terminal").append("<br><span class='terminal-red'>Sorry, this command has not yet been implemented</span>");	
 			}else if (text.indexOf("cd about")<2 && text.indexOf("cd about")>-1){   
-				$("#terminal").append("<br><span class='terminal-red'>about not inplemented yet</span>");
+				$("#terminal").append("<br><span class='terminal-red'>Name: William da Cunha Ribeiro</span>");
+				$("#terminal").append("<br><span class='terminal-red'>Nationality: Brazilian</span>");
+				$("#terminal").append("<br><span class='terminal-red'>Phone:+55 (84)9 9214-3773</span>");
+				$("#terminal").append("<br><span class='terminal-red' ><a href='https://github.com/willcribeiro'' class='terminal-red'>Github</a></span>");
+				$("#terminal").append("<br><span class='terminal-red' ><a href='https://www.linkedin.com/in/willcr/'' class='terminal-red'>Linkedin</a></span>");
+				$("#terminal").append("<br><span class='terminal-red'>E-Mail: william-cribeiro@bct.ect.ufrn.br/williamribeiro00@gmail.com</span>");
+				$("#terminal").append("<br><span class='terminal-red'>For more details go to my <a href='index.html'' class='terminal-red'> homepage</a> :) </span>");
+					
 			}else{
-				$("#terminal").append("<br><span class='terminal-red'>command not found</span>");	
+				$("#terminal").append("<br><span class='terminal-red'>sorry, command not found. Try see help comands :)</span>");	
 			}
 
 			$("#terminal").append('<br><span class="terminal-green">viewer@outsider:~$ </span> <span class="terminal-green"></span>');
